bf3a458ab39d023aab9ce6df5aca2033c50d4180
AthmikaU <athmikaubhat@gmail.com>
2025-11-13 21:24:49 +0530

    updated API_URL to deployed backend

--- emergency-tracker/src/api.js
+++ emergency-tracker/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // deployed backend URL when hosted
-export const API_URL = "http://localhost:8000";
+// export const API_URL = "http://localhost:8000";
+export const API_URL = "https://lora-tracker.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL,
